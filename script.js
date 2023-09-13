53098c2e804415f2e7026bf485d5061ac5bce614
Munique Feitoza <muniquefeitoz4@gmail.com>
2023-09-13 18:59:06 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+let btnLC = document.querySelector('.btn-LoginCadastro');
+let formL = document.getElementById('login');
+let formC = document.getElementById('cadastro');
+
+btnLC.addEventListener('click', function() {
+  if(formL.style.display != 'block') {
+    formL.style.display = 'block';
+    formC.style.display = 'none';
+    btnLC.innerText = "Login";
+    document.querySelector('.btn-LoginCadastro').style.marginLeft = "11vw";
+    return;
+  } else {
+  formL.style.display = 'none';
+  formC.style.display ='block';
+  btnLC.innerText = "Cadastro";
+  document.querySelector('.btn-LoginCadastro').style.marginLeft = "6vw";
+  return;
+  }
+ }
+);
