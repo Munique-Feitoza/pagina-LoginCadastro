ae96918ab7d2127a405e82fe7e0bdf74b5059cc1
muniquefeitoz4@gmail.com <muniquefeitoz4@gmail.com>
2023-10-17 22:52:40 -0300

    adicionando uma versão para computador e deixando a página login/cadastro mais acessível tanto para celular quanto para computador

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,8 @@ let btnLC = document.querySelector('.btn-LoginCadastro');
 let formL = document.getElementById('login');
 let formC = document.getElementById('cadastro');
 let myBody = document.querySelector('body');
+let text1 = document.querySelector('#texto1');
+let text2 = document.querySelector('#texto2');
 
 btnLC.addEventListener('click', function() {
   if(formL.style.display != 'block') {
@@ -10,14 +12,18 @@ btnLC.addEventListener('click', function() {
     btnLC.innerText = "Login";
     document.querySelector('.btn-LoginCadastro').style.marginLeft = "11vw";
     myBody.style.backgroundColor = "black";
+    text1.style.color = "white";
+    text2.style.color = "white";
     return;
   } else if (formC.style.display != 'block'){
-  formL.style.display = 'none';
-  formC.style.display ='block';
-  btnLC.innerText = "Cadastro";
-  document.querySelector('.btn-LoginCadastro').style.marginLeft = "6vw";
-  myBody.style.backgroundColor = "black";
-  return;
+    formL.style.display = 'none';
+    formC.style.display ='block';
+    btnLC.innerText = "Cadastro";
+    document.querySelector('.btn-LoginCadastro').style.marginLeft = "11vw";
+    myBody.style.backgroundColor = "black";
+    text1.style.color = "white";
+    text2.style.color = "white";
+    return;
   }
  }
 );
